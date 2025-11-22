4dd0c7bf535355c87d5eb91a78e6b0c1df637ebd
Саша <139625754+Sasha201089@users.noreply.github.com>
2025-11-22 10:24:09 +0300

    Update EventSearcher.py

--- EventDataBase/EventSearcher.py
+++ EventDataBase/EventSearcher.py
@@ -1,24 +1,26 @@
 from openai import OpenAI
 import json
 import Manager
+def write_new_events():
+  client = OpenAI(
+    api_key="my_code"
+  )
+  
+  response = client.responses.create(
+    model="gpt-5-nano",
+    input='''Проанализируй эту веб страницу: https://it-event-hub.ru/ и напиши информацию только о мероприятиях, проходящих
+          в Санкт-Петербурге. Ответ выведи в таком формате словаря без стороннего форматирования: {"{название мероприятия}" : {"место проведения" : "{место проведения}",
+          "дата проведения" : "{дата проведения}", "тип" : "{тип мероприятия}", "стоимость" : "{стоимость, если есть}",
+          "описание" : "{описание мероприятия}", "ссылка" : "{ссылка на мероприятие}"}, и т. д.}
+          ''',
+    tools=[{"type": "web_search_preview"}],
+    store=True,
+  )
+  text = response.output_text
+  print(text)
+  text1 = json.load(text)
+  for t in text1:
+    Manager.write_in_base(t, text1[t])
+  return True
 
-client = OpenAI(
-  api_key="my_code"
-)
-
-response = client.responses.create(
-  model="gpt-5-nano",
-  input='''Проанализируй эту веб страницу: https://it-event-hub.ru/ и напиши информацию только о мероприятиях, проходящих
-        в Санкт-Петербурге. Ответ выведи в таком формате словаря без стороннего форматирования: {"{название мероприятия}" : {"место проведения" : "{место проведения}",
-        "дата проведения" : "{дата проведения}", "тип" : "{тип мероприятия}", "стоимость" : "{стоимость, если есть}",
-        "описание" : "{описание мероприятия}", "ссылка" : "{ссылка на мероприятие}"}, и т. д.}
-        ''',
-  tools=[{"type": "web_search_preview"}],
-  store=True,
-)
-text = response.output_text
-print(text)
-text1 = json.load(text)
-for t in text1:
-  Manager.write_in_base(t, text1[t])
 
